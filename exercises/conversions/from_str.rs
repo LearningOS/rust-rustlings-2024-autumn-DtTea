8a68e97279f28554db4be269cadea8e724f7f25f
DtTea <1685181965@qq.com>
2024-10-06 22:26:21 +0800

    20241006

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,7 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
+
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +52,30 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.trim().len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+
+        let vec:Vec<&str> = s.split(",").collect();
+
+        if vec.len() != 2 {
+            return Err(ParsePersonError::BadLen)
+        }
+
+        let name = String::from(vec[0]);
+
+        if name.len() == 0 {
+            return Err(ParsePersonError::NoName);
+        }
+
+        let age = match vec[1].parse::<usize>() {
+            Ok(age) => age, Err(e) => return Err(ParsePersonError::ParseInt(e))
+        };
+
+        Ok(Person{
+            name,
+            age
+        })
     }
 }
 
